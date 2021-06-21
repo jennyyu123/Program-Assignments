53c0c39500abd4b36b85a1fcf8e0fac0f9dc4f61
jennyyu123 <85902089+jennyyu123@users.noreply.github.com>
2021-06-21 00:02:57 -0700

    Add files via upload

---
+++ dictionary_practice.py
@@ -0,0 +1,34 @@
+groceries_list = {"chicken":1.59, "beef": 1.99, "cheese": 1.00, "milk" : 2.50}
+NBA_players = {"Lebron James": 23, "Kevin Durrant":35, "Stephen Curry": 30, "Damian Lillard" :0  }
+phone_numbers = {"friend1" : 1234567, "friend2": 2942784, "friend3": 2849472, "friend4": 23927429}
+shoe_inventory={"jordan13": 1,"yeezy" :8, "foamposite": 10, "airmax": 5, "sbdunk": 20}
+
+
+NBA_players["Lebron James"] = 6 
+jersey_number=NBA_players ["Lebron James"]
+print(jersey_number)
+
+NBA_players["Lebron James"] -= 17
+jersey_number=NBA_players ["Lebron James"]
+print (jersey_number)
+
+chicken_price = groceries_list ["chicken"]
+print (chicken_price)
+
+beef_price = groceries_list ["beef"]
+print (beef_price)
+
+cheese_price = groceries_list ["cheese"]
+print (cheese_price)
+
+milk_price = groceries_list ["milk"]
+print (milk_price)
+
+friend1_number = phone_numbers[friend1]
+print (friend1_number)
+friend2_number = phone_numbers[friend2]
+print (friend2_number)
+friend3_number = phone_numbers[friend3]
+print (friend3_number)
+friend4_number = phone_numbers[friend4]
+print (friend4_number)

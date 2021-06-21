a674f83fcae5f36f1b1e303e198873c4b4eb21ae
jennyyu123 <85902089+jennyyu123@users.noreply.github.com>
2021-06-21 00:04:16 -0700

    Add files via upload

---
+++ lab_2.py
@@ -0,0 +1,19 @@
+print ("Hello World")
+
+name = "LeBron"
+#type is text sequence
+age = 35
+#type is an integer
+salary = 37.44
+#type is a float
+plays_basketball = True
+#type is a boolean
+jersey_number = "23"
+#type is an integer
+
+car_name = "Ford"
+x = 50
+first_name = "Jenny"
+print (car_name)
+type (x)
+print (first_name)

6b7db526131301957416711ce4b53c661a20d216
jennyyu123 <85902089+jennyyu123@users.noreply.github.com>
2021-06-21 00:14:14 -0700

    Add files via upload
    making lists!

---
+++ list_practice.py
@@ -0,0 +1,20 @@
+empty_list=[]
+#               0         1          2            3             4         5         6              7            8      
+city_list=["Oakland", "Atlanta", "New York City", "Seattle", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+
+
+#changing the cities
+city_list[0]= "San Francisco"
+city_list[2]="Brooklyn"
+city_list[6]="Hollywood"
+city_list[4]= "Tampa"
+
+city_list.append ("New Jersey")
+city_list.extend (["Santa Cruz", "Selma", "Chicago"])
+city_list.insert( 7, "Washington, D.C.")
+print (city_list)
+
+#                0    1    2    3    4    5    6   7    8    9    10
+alphabet_list= ["a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k"]
+print (alphabet_list)
+
